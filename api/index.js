c37f66a0cbf2c5f435a42dd437aebaaccda7c295
Merkaln_Bogdan <piccasoul19@ukr.net>
2022-12-20 23:12:21 +0200

    add cors rule

--- api/index.js
+++ api/index.js
@@ -26,7 +26,9 @@ module.exports = class UserList {
 
   initMiddlewares() {
     this.server.use(express.json());
-    this.server.use(cors({ origin: "http://localhost:3000" }));
+    this.server.use(
+      cors({ origin: "https://merkaln-register-phonebook.netlify.app" })
+    );
     this.server.use(express.static("static"));
   }
   initUserRoutes() {
